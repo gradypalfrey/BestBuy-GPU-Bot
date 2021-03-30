146b45af1253648d290a3092d154701d337d1a8a
gpbio10 <40411387+gpbio10@users.noreply.github.com>
2021-03-30 14:12:23 -0400

    Added multiple card support

--- main.py
+++ main.py
@@ -5,35 +5,63 @@ from datetime import datetime
 
 from twilio.rest import Client
 
+import random
+
 browser = webdriver.Firefox(executable_path=r'C:/Users/grady/Downloads/geckodriver-v0.29.0-win64/geckodriver')
 
 # Twilio
-number = "+16043121279"
-fromNumber = "+17034680663"
-accountSID = "AC49c954e929d3f6541a0b255098c11f39"
-authToken = "9af27e4e206e9abf1a6e3e47f90d44d3"
+number = ""
+fromNumber = ""
+accountSID = ""
+authToken = ""
 client = Client(accountSID, authToken)
 
 
-def bestBuy(card, n):
-    bestbuy_status = browser.find_element_by_xpath("/html/body/div[1]/div/div[4]/div[1]/div[2]/div[2]/div[4]/div/div[1]/div/p/span").get_attribute("innerHTML")
-    now = datetime.now().time()
-    if bestbuy_status == "Available to ship":
-        client.messages.create(to=number, from_=fromNumber, body="Buy a 3060-ti!")
-        return str(n) + ". BUY BUY BUY " + card + " " + str(now)
-    else:
-        return str(n) + ". out of stock. " + str(now)
+class Card:
+    def __init__(self, name, link, status):
+        self.name = name
+        self.link = link
+        self.status = status
+
+    def best_buy(self, n):
+        time.sleep(random.randint(3, 6))
+        bestbuy_status = browser.find_element_by_xpath(
+            "/html/body/div[1]/div/div[4]/div[1]/div[2]/div[2]/div[4]/div/div[1]/div/p/span").get_attribute("innerHTML")
+        now = datetime.now().time()
+        if bestbuy_status == "Available to ship":
+            self.status = True
+            client.messages.create(to=number, from_=fromNumber, body="Buy a " + self.name + " at " + self.link)
+            return str(n) + "\t\tBuy a " + self.name + " at " + self.link + "\t\t" + str(now)
+        else:
+            return str(n) + "\t\t" + self.name + ":\t" + bestbuy_status + "\t\t" + str(now)
 
 
 def main():
-    card = "3060ti"
-    browser.get("https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285")
-    # browser.get("https://www.bestbuy.ca/en-ca/product/amd-ryzen-7-5800x-octa-core-3-8ghz-am4-desktop-processor/15331715")
+
+    nvidia = Card("Nvidia", "https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285", False)
+    evga = Card("Evga", "https://www.bestbuy.ca/en-ca/product/evga-nvidia-geforce-rtx-3060-ti-ftw3-ultra-8gb-gddr6-video-card/15229237", False)
+    msi = Card("MSI", "https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3060-ti-ventus-2x-oc-8gb-gddr6-video-card/15178453", False)
+    # zotac = Card("Zotac", "https://www.bestbuy.ca/en-ca/product/zotac-geforce-rtx-3060-ti-twin-edge-oc-8gb-gddr6-video-card/15178452", False)
+    tuff = Card("Tuff", "https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-geforce-rtx-3060-ti-oc-8gb-gddr6-video-card/15201200", False)
+
     n = 1
     while True:
-        browser.refresh()
-        print(bestBuy(card, n))
-        time.sleep(5)
+        browser.get(nvidia.link)
+        print(nvidia.best_buy(n))
+
+        browser.get(evga.link)
+        print(evga.best_buy(n))
+
+        browser.get(msi.link)
+        print(msi.best_buy(n))
+
+        # browser.get(zotac.link)
+        # print(zotac.best_buy(n))
+
+        browser.get(tuff.link)
+        print(tuff.best_buy(n))
+
         n += 1
+
 
 main()
